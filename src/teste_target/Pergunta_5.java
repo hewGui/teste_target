9965cdeafabf863b8b82a6b673222e2a24c24ad9
Guilherme <guilhermesoaresilvat@gmail.com>
2024-09-02 09:53:49 -0300

    Questão 5

---
+++ src/teste_target/Pergunta_5.java
@@ -0,0 +1,31 @@
+package teste_target;
+
+import java.util.Scanner;
+
+public class Pergunta_5 {
+	
+	    public static void main(String[] args) {
+	     
+	        Scanner scanner = new Scanner(System.in);
+
+	     
+	        System.out.print("Digite uma string: ");
+	        String original = scanner.nextLine();
+
+	        
+	        String invertida = "";
+
+	        
+	        for (int i = original.length() - 1; i >= 0; i--) {
+	            invertida += original.charAt(i);  
+	        }
+
+	
+	        System.out.println("String invertida: " + invertida);
+
+	        
+	        scanner.close();
+	    }
+	}
+
+
